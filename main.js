a68e65dca49ec15343c0887ae7457c3717a8d6b3
Darlin <darli2819@gmail.com>
2024-10-24 08:58:36 -0400

    begining of win conditions

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let currentPlayer = "X";
 let gameBoard = ["", "", "", "", "", "", "", "", ""];
-
+let cells = document.querySelectorAll('.cell');
 let gameActive = true;
 
 function handlePlayerTurn (clickedCellIndex) {
@@ -8,13 +8,23 @@ function handlePlayerTurn (clickedCellIndex) {
         return;
     }
 
-    gameBoard[clickedCellIndex] =currentPlayer;
+    gameBoard[clickedCellIndex] = currentPlayer;
     checkForWinOrDraw();
-
-    if (currentPlayer === "X") {
-        currentPlayer = "O";
-    } else {
-        currentPlayer = "X";
-    }
+    currentPlayer = currentPlayer == "x" ? "o" : "x"// if x went then o will go
+    
 }
+
+function checkForWinOrDraw(){
+    let winCondition = [[0, 1, 2],//first column
+                        [3, 4, 5],//second column
+                        [6, 7, 8],//third column
+                        [0, 3, 6],//top row
+                        [1, 4, 7],//second row
+                        [2, 5, 8],//third row
+                        [0, 4, 8],//left diagonal
+                        [2, 4, 6]];//right diagonal
+    
+                                
+}
+    
 
